032111aa874a97d5b3c62c0a4b42a4c22d6bc1ab
hasanrd212-lang <hasan.rd212@gmail.com>
2026-09-14 18:24:08 +0300

    Fix: Cast Double to Int in ChessEngine.swift
    Line 61 was trying to assign a Double value to an Int variable without explicit casting.
    Added Int() cast to properly convert cp (Double) to centipawns (Int).

--- Sources/ChessCoach/ChessEngine.swift
+++ Sources/ChessCoach/ChessEngine.swift
@@ -58,7 +58,7 @@ final class ChessEngine: ObservableObject {
             case .info(let info):
                 if let score = info.score {
                     if let cp = score.cp {
-                        centipawns = Int(cp)
+                        centipawns = Int(cp) // cp is Double, cast to Int
                         mateIn = nil
                     } else if let mate = score.mate {
                         mateIn = mate
